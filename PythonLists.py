838d58bc9ea2720e1a708bc22599e55a62318f55
JamalE101 <jamy101@live.co.uk>
2022-01-31 16:25:45 +0000

    commit

---
+++ PythonLists.py
@@ -0,0 +1,2 @@
+lucky_numbers = [5, 55, 4, 3, 101, 42]
+print(min(lucky_numbers))
--- collections.py
+++ collections.py
@@ -1,7 +1,17 @@
-shoppinglist = ["meat", "veg", "cake", "cola for the weekend"]
-cool_cows = ["Winnie the Moo", "Moolan", "Milkshake", "Mooana"]
-cool_sheep = ["Baaaart", "Baaaarnaby"]
-cool_pigs = ["Chris P. Bacon", "Hamlet", "Hogwarts"]
-cool_animals = [cool_cows, cool_sheep, cool_pigs]
+# shoppinglist = ["meat", "veg", "cake", "cola for the weekend"]
+# cool_cows = ["Winnie the Moo", "Moolan", "Milkshake", "Mooana"]
+# cool_sheep = ["Baaaart", "Baaaarnaby"]
+# cool_pigs = ["Chris P. Bacon", "Hamlet", "Hogwarts"]
+# cool_animals = [cool_cows, cool_sheep, cool_pigs]
 
-print
+
+books = {"The Handmaiden's Tale":"Margaret Atwood", "The Hobbit":"J.R.R. Tolkien", "Charlie and the Chocolate Factory":"Roald Dahl"}
+print(books["The Hobbit"])
+
+# Remove the last element of the list 
+lst=[55, 777, 54, 6, 76, 101, 1, 2, 8679, 123, 99]
+
+
+lst.remove(99)
+
+print(lst)

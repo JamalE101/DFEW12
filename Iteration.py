d5939417d9e9b43f48cd80cc17f4d1d7265e71ec
JamalE101 <jamy101@live.co.uk>
2022-02-01 11:44:08 +0000

    commit

---
+++ Iteration.py
@@ -0,0 +1,14 @@
+# countdown
+
+# var = 5
+
+# while var > 0:
+#     print(var)
+#     var = var -1 
+# var = int(input("enter a number:"))
+# while var > 0 and var <2000:
+#     if var%2 == 0: 
+#      var = var -1
+
+for i in range(5):
+    print("output", i)
